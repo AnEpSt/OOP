542511d6c05738c2bff3fc0970c485c40be0e2a6
AnEpSt <epihin3000@gmail.com>
2025-02-26 18:56:08 +0300

    Create mainwindow.cpp

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "csvreader.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    CsvReader reader ("music.csv");
+
+    if (reader.is_open()) {
+        std::vector<music> mus = reader.readAll();
+
+        for (const auto& c: books) {
+            ui->textBrowser->append(QString::fromStdString(c.name()) + " " + QString::fromStdString(c.author())+ " " + QString::number(c.year()) + " " + QString::number(c.proslushivanie()));
+        }
+    } else {
+        ui->textBrowser->append("Ошибка при открытии файла");
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
