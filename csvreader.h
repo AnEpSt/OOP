a994f71c89bb60633b50580f1f8453f3db7dc64e
AnEpSt <epihin3000@gmail.com>
2025-02-26 18:58:23 +0300

    Create csvreader.h

---
+++ csvreader.h
@@ -0,0 +1,20 @@
+#ifndef CSVREADER_H
+#define CSVREADER_H
+
+#include <string>
+#include <vector>
+#include "music.h"
+#include <fstream>
+
+class CsvReader
+{
+public:
+    CsvReader(const std::string& filename);
+    bool is_open() const;
+    std::vector<music> readAll();
+
+private:
+    std::ifstream fin;
+};
+
+#endif // CSVREADER_H
