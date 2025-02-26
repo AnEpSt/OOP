b108ebffc3f3bdbddb1dc100bd7a697685eab620
AnEpSt <epihin3000@gmail.com>
2025-02-26 19:00:08 +0300

    Create csvreader.cpp

---
+++ csvreader.cpp
@@ -0,0 +1,44 @@
+#include "csvreader.h"
+#include <fstream>
+
+CsvReader::CsvReader(const std::string& filename)
+{
+    fin.open(filename);
+}
+
+bool CsvReader::is_open() const
+{
+    return fin.is_open();
+}
+
+std::vector<music> CsvReader::readAll()
+{
+    std::vector<music> mus;
+
+    while (!fin.eof()) {  // eof - end of file
+        std::string line;
+        std::getline(fin, line);
+
+        mus c;
+        int k = 0;
+        std::string token;
+
+        for(int i = 0; i < line.size(); ++i) {
+            if(line[i] != ';') {
+                token += line[i];
+            }
+            else {
+                switch(k) {
+                case 0: c.setName(token); token.clear(); k++; break;
+                case 1: c.setAuthor(token); token.clear(); k++; break;
+                case 2: c.setYear(std::stoi(token)); token.clear(); k++; break;
+                case 3: c.setprosl(std::stoi(token)); token.clear(); k++; break;
+                }
+                // break;
+            }
+        }
+        mus.push_back(c);
+    }
+
+    return mus;
+}
