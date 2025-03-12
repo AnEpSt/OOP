2680c8a789c94fba70295835efec8db8d85a5e8d
AnEpSt <epihin3000@gmail.com>
2025-03-12 19:40:26 +0300

    Update csvreader.cpp

--- csvreader.cpp
+++ csvreader.cpp
@@ -11,13 +11,13 @@ bool CsvReader::is_open() const
     return fin.is_open();
 }
 
-std::vector<music> CsvReader::readAll()
+std::vector<music> CsvReader::readALL()
 {
     std::vector<music> mus;
-
-    while (!fin.eof()) {  // eof - end of file
+    while (!fin.eof()) {
         std::string line;
         std::getline(fin, line);
+        if (line.empty()) continue;
 
         music c;
         int k = 0;
@@ -29,16 +29,16 @@ std::vector<music> CsvReader::readAll()
             }
             else {
                 switch(k) {
-                case 0: c.setName(token); token.clear(); k++; break;
-                case 1: c.setAuthor(token); token.clear(); k++; break;
-                case 2: c.setYear(std::stoi(token)); token.clear(); k++; break;
-                case 3: c.setprosl(std::stoi(token)); token.clear(); k++; break;
+                case 0: c.setName(token); break;
+                case 1: c.setAuthor(token); break;
+                case 2: c.setYear(std::stoi(token)); break;
+                case 3: c.setprosl(std::stoi(token)); break;
                 }
-                // break;
+                token.clear();
+                k++;
             }
         }
         mus.push_back(c);
     }
-
     return mus;
 }
