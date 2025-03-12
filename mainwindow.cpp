042b0878f0c3847d7290f40b0d9dd17ca23863cd
AnEpSt <epihin3000@gmail.com>
2025-03-12 13:33:56 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,27 +1,58 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "csvreader.h"
-
+#include "csvwriter.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    CsvReader reader ("music.csv");
-
-    if (reader.is_open()) {
-        std::vector<music> mus = reader.readAll();
-
-        for (const auto& c: mus) {
-            ui->textBrowser->append(QString::fromStdString(c.name()) + " " + QString::fromStdString(c.author())+ " " + QString::number(c.year()) + " " + QString::number(c.proslushivanie()));
+}
+    //ui->textBrowser->append("Hello QT");
+ void MainWindow::LoadData(){
+     ui->textBrowser->clear();
+    CsvReader reader("music.csv");// open file
+    if (reader.is_open()){
+        std::vector<music> vec = reader.readALL();
+        for (const auto& m:vec){
+            ui->textBrowser->append(QString::fromStdString(m.name())+ " " + QString::number(m.year()) + " " +QString::number(m.proslushivanie()) + " " + QString::fromStdString(m.author()));
         }
-    } else {
+        }
+    else{
         ui->textBrowser->append("Ошибка при открытии файла");
     }
-}
+    }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::add(){
+
+    std::vector<music> vec;
+    CsvWriter writer("music.csv");
+    if (writer.is_open()){
+        QString line = ui->linesearch->text();
+        QString line1 = ui->linesearch1->text();
+        QString line2 = ui->linesearch2->text();
+        QString line3 = ui->linesearch3->text();
+
+        QStringList d    = line.split(" ");
+
+        music m(line.toStdString(),line1.toInt(),line2.toInt(),line3.toStdString());
+        writer.write(m);
+        LoadData();
+
+    CsvReader reader("music.csv");
+    if (reader.is_open()){
+        std::vector<music> vec = reader.readALL();
+        for (const auto& m:vec){
+            ui->textBrowser->append(QString::fromStdString(m.name())+ " " + QString::number(m.year()) + " " +QString::number(m.proslushivanie()) + " " + QString::fromStdString(m.author()));
+        }
+    }
+    else{
+        ui->textBrowser->append("Ошибка при открытии файла");
+    }
+
+}
 }
