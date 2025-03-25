df52450c3f9e70d4a79a55dc5148a8ad6081542c
AnEpSt <epihin3000@gmail.com>
2025-03-25 22:05:28 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,20 +2,22 @@
 #include "./ui_mainwindow.h"
 #include "csvreader.h"
 #include "csvwriter.h"
+#include "MyTextbrowser.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    LoadData();
 }
     //ui->textBrowser->append("Hello QT");
  void MainWindow::LoadData(){
      ui->textBrowser->clear();
     CsvReader reader("music.csv");// open file
     if (reader.is_open()){
-        std::vector<music> vec = reader.readALL();
+        vec = reader.readALL();
         for (const auto& m:vec){
-            ui->textBrowser->append(QString::fromStdString(m.name())+ " " + QString::number(m.year()) + " " +QString::number(m.proslushivanie()) + " " + QString::fromStdString(m.author()));
+            ui->textBrowser->appendmusic(m);
         }
         }
     else{
@@ -28,31 +30,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 void MainWindow::add(){
-
-    std::vector<music> vec;
     CsvWriter writer("music.csv");
+    QString name = ui->linesearch->text();
+    QString author = ui->linesearch1->text();
+    int year = ui->linesearch2->text().toInt();
+    int proslushivanie = ui->linesearch3->text().toInt();
+    QString genre = ui->linesearch4->text();
+    QString feat = ui->linesearch5->text();
+    remix m;
+    vec.push_back(m);
     if (writer.is_open()){
-        QString line = ui->linesearch->text();
-        QString line1 = ui->linesearch1->text();
-        QString line2 = ui->linesearch2->text();
-        QString line3 = ui->linesearch3->text();
+        remix m(name.toStdString(),genre.toStdString(),feat.toStdString());
 
-        QStringList d    = line.split(" ");
-
-        music m(line.toStdString(),line1.toInt(),line2.toInt(),line3.toStdString());
         writer.write(m);
-        LoadData();
-
-    CsvReader reader("music.csv");
-    if (reader.is_open()){
-        std::vector<music> vec = reader.readALL();
-        for (const auto& m:vec){
-            ui->textBrowser->append(QString::fromStdString(m.name())+ " " + QString::number(m.year()) + " " +QString::number(m.proslushivanie()) + " " + QString::fromStdString(m.author()));
-        }
-    }
-    else{
-        ui->textBrowser->append("Ошибка при открытии файла");
-    }
-
+//LoadData();
+        ui->textBrowser->appendmusic(m);
 }
 }
