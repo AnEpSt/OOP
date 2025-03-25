9ec2e729322e7916d9e3610f4d59bdd6092408d2
AnEpSt <epihin3000@gmail.com>
2025-03-25 21:57:52 +0300

    Create abstractreader.h

---
+++ abstractreader.h
@@ -0,0 +1,14 @@
+#ifndef ABSTRACTREADER_H
+#define ABSTRACTREADER_H
+#include <vector>
+#include <string>
+#include "remix.h"
+
+class AbstractReader {
+public: 
+    virtual ~AbstractReader(){}
+    virtual std::vector<remix> readALL = 0;
+    virtual bool is_open const=0;
+}
+
+#endif // ABSTRACTREADER_H
