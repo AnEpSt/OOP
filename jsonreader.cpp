95b2652fc5c693c9cec0c466e3f2e8687d77553e
AnEpSt <epihin3000@gmail.com>
2025-04-01 19:26:58 +0300

    Update jsonreader.cpp

--- jsonreader.cpp
+++ jsonreader.cpp
@@ -1,26 +1,43 @@
-#includde "jsonreader.h"
+#include "jsonreader.h"
 #include  <fstream>
+#include "remix.h"
 
 JsonReader::JsonReader(const std::string& filename){
-  fin.open(filename);
+    fin.open(filename);
 }
 bool JsonReader::is_open() const{
-  return bool.is_open();
+    return fin.is_open();
 }
-std::vector<remix> JsonRader::ReadALL(){
-  std::vector<remix> rem;
-  while (!fin.eof()){
-      json j;
-      fin >> j;
-      for (const auto& e: j) {
+std::vector<remix> JsonReader::readALL(){
+    std::vector<remix> rem;
+    std::string name;
+    std::string author;
+    int year;
+    int prosl;
+    std::string genre;
+    std::string feat;
+    // while (!fin.eof()){
+        json j;
+        fin >> j;
         remix c;
-        e["name"].get_to(c.name);
-        e["author"].get_to(c.author);
-        e["year"].get_to(c.year);
-        e["prosl"].get_to(c.prosl);
-        e["genre"].get_to(c.genre);
-        e["feat"].get_to(c.feat);
+        for (const auto& e: j) {
+
+            e["name"].get_to(name);
+            e["author"].get_to(author);
+            e["year"].get_to(year);
+            e["prosl"].get_to(prosl);
+            e["genre"].get_to(genre);
+            e["feat"].get_to(feat);
+
+            c.setName(name);
+            c.setAuthor(author);
+            c.setprosl(prosl);
+            c.setYear(year);
+            c.setgenre(genre);
+            c.setfeat(feat);
+            rem.push_back(c);
         }
-      rem.push_back(c);
-  }
+
+    // }
+        return rem;
 }
