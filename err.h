8d407eccda4130f56cdb5470b193306c692649e7
AnEpSt <epihin3000@gmail.com>
2025-04-01 20:27:49 +0300

    Update err.h

--- err.h
+++ err.h
@@ -1,1 +1,16 @@
+#ifndef ERR.H
+#define ERR_H
+#include <iostream>
+#include <exception>
+class err:public exception{
+    private:
+        std::string err_message ="Исключений не обнаружено";
+        int str_numb = 0;
+    public: 
+        err() = default
+        err(const std::string& mes, int numb): err_message(mes), str_numb(numb){}
 
+
+
+
+}
