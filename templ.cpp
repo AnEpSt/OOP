0ba4576075a0011bde5561801d6efa0f8b70d286
AnEpSt <epihin3000@gmail.com>
2025-04-22 20:47:07 +0300

    update templates  templ.cpp

--- templ.cpp
+++ templ.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 #include <vector>
+void print_vec(const std::vector<int>& vec){
+  for (const auto& elem : vec) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+
 template<typename T> T avg(T a, T b)
 {
     return (a + b)/2;
 }
 
-template<typename T> void mult(std::vector<T>& vec, T value){
-  for (const auto& element : vec) {
+template<typename T> std::vector<T> mult(std::vector<T>& vec, T value){
+  for ( auto& element : vec) {
           element *= value;
       }
+  return vec;
 }
-template<typename T> void inv(std::vector<T>& vec){
-  for (const auto& element : vec) {
+template<typename T> std::vector<T> inv(std::vector<T>& vec){
+  for (auto& element : vec) {
           element *= -1;
       }
+  return vec;
 }
-template<typename T> void plus(std::vector<T>& vec,T value){
-  for (const auto& element : vec) {
+template<typename T> std::vector<T> plus(std::vector<T>& vec,T value){
+  for ( auto& element : vec) {
           element += value;
       }
+  return vec;
 }
-int main(int argc, char* argv[])
+
+int main()
 {
+    std::vector vec= {5,79,13,158,466};
     double x = avg(5, 4);
-    std::vector vec= {5,564,45,456,654};
-    mult(vec,4545);
-    inv(vec);
-    plus(vec,45854654);
+    // mult(vec,4545);
+    std::cout<<x<<std::endl;
+    
+    print_vec(mult(vec,796));
+    print_vec(inv(vec));
+    print_vec(plus(vec,798));
+    
+    // std::cout << std::endl;
+    // std::cout<<res1 <<std::endl;
+    // std::cout<<<<std::endl;
+    // std::cout<<plus(vec,45854654)<<std::endl;
+   
+    
+    
 }
