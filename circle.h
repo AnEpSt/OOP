85f8bcccfa7f08c8297247de61a8d948d6204ac2
AnEpSt <epihin3000@gmail.com>
2025-05-04 15:05:58 +0300

    Update circle.h

--- circle.h
+++ circle.h
@@ -1,14 +1,14 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 #include "Figure.h"
+#include <QPoint>
 class Circle : public Figure{
 private:
-  Qpoint cir_c0entre;
+  Qpoint cir_centre;
   int cir_rad;
 public:
-  Circle(const QJsonObject& json);
+  Circle(const QPoint& cir_centre, int cir_rad);
   void paint(QPainter* painter) const override;
-  QJsonObject toJson() const override;
 
 };
 #endif // CIRCLE_H
