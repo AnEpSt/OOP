c3d02f4b1bdcdb407d95522ca1414fd4a7cc0f72
AnEpSt <epihin3000@gmail.com>
2025-05-05 19:22:38 +0300

    Update rectangle.h

--- rectangle.h
+++ rectangle.h
@@ -2,11 +2,13 @@
 #define RECTANGLE_H
 #include "Figure.h"
 #include <QRect>
-class Rectangle : public Figure{
-private:
-  QRect m_rect;
+
+class Rectangle : public Figure {
 public:
-  Rectangle(const QRect& rect);
-  void paint(QPainter* painter) const override;
-}
+    Rectangle(const QRect& rect);
+    void paint(QPainter* painter) const override;
+
+private:
+    QRect m_rect;
+};
 #endif // RECTANGLE_H
