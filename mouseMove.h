ee6c3baf980166bc5ecd73368364017c8e4ee84b
AnEpSt <epihin3000@gmail.com>
2025-05-12 22:11:51 +0300

    Create mouseMove.h

---
+++ mouseMove.h
@@ -0,0 +1,14 @@
+#ifndef MOUSEMOVE_H
+#define MOUSEMOVE_H
+#include <QWidget>
+#include <QMouseEvent>
+
+class mouseMove: public QWidget {
+public:
+    explicit MyWidget(QWidget* parent = nullptr);
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+};
+
+#endif // MOUSEMOVE_H
