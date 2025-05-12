8bcfdda7dd7e12c94aefebbb1d7082340984963f
AnEpSt <epihin3000@gmail.com>
2025-05-12 22:39:29 +0300

    Update mouseMove.h

--- mouseMove.h
+++ mouseMove.h
@@ -1,14 +1,23 @@
 #ifndef MOUSEMOVE_H
 #define MOUSEMOVE_H
+
 #include <QWidget>
 #include <QMouseEvent>
+#include <vector>
+#include <memory>
+#include "Figure.h"
 
-class mouseMove: public QWidget {
+class mouseMove : public QWidget {
+private:
+    std::vector<std::unique_ptr<Figure>> figures;
+    Figure* highlightedFigure = nullptr;
 public:
     explicit mouseMove(QWidget* parent = nullptr);
-    virtual void mouseMoveEvent(QMouseEvent *event) override;
-    virtual void mousePressEvent(QMouseEvent *event) override;
-    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+    void addFigure(std::unique_ptr<Figure> figure);
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+
+
 };
 
 #endif // MOUSEMOVE_H
